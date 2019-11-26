630f4f829fda12908428d04995000815118df17d
TsengYiYao <yiyatsen@gmail.com>
2019-11-26 18:03:49 +0800

    finish ep.32

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,33 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+//        appBar: AppBar(
+//          title: Text('Demo'),
+//        ),
+        body: SafeArea(
+          child: Container(
+            height: 100.0,
+            width: 100.0,
+            margin: EdgeInsets.only(left: 30.0),
+            padding: EdgeInsets.all(20.0),
+            color: Colors.white,
+            child: Text('Hello'),
+          ),
+        ),
+//        floatingActionButton: FloatingActionButton(
+//          backgroundColor: Colors.red,
+//          child: Icon(Icons.add),
+//        ),
       ),
-    ),
-  );
+    );
+  }
 }
