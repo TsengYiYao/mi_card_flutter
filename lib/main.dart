180dadda3d34c66be174f74dd9ef514d41b90f00
TsengYiYao <yiyatsen@gmail.com>
2019-11-27 17:30:43 +0800

    finished ch6 ep.38

--- lib/main.dart
+++ lib/main.dart
@@ -10,23 +10,101 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-//        appBar: AppBar(
-//          title: Text('Demo'),
-//        ),
         body: SafeArea(
-          child: Container(
-            height: 100.0,
-            width: 100.0,
-            margin: EdgeInsets.only(left: 30.0),
-            padding: EdgeInsets.all(20.0),
-            color: Colors.white,
-            child: Text('Hello'),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+//            mainAxisSize: MainAxisSize.min,
+
+            children: <Widget>[
+              CircleAvatar(
+                radius: 50,
+                backgroundImage: AssetImage('images/manipulatedelephant.jpg'),
+              ),
+              Text(
+                'Elephant',
+                style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                'Flutter Developer',
+                style: TextStyle(
+                    fontFamily: 'Source Sans Pro',
+                    fontSize: 20,
+                    color: Colors.teal.shade100,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.5),
+              ),
+              SizedBox(
+                height: 20,
+                width: 150,
+                child: Divider(
+                  color: Colors.teal.shade100,
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+886 0938 438 438',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'iamgod6666@gmail.com',
+                    style: TextStyle(
+                      color: Colors.teal.shade900,
+                      fontFamily: 'Source Sans Pro',
+                      fontSize: 20,
+                    ),
+                  ),
+                ),
+              ),
+//              Card(
+//                color: Colors.white,
+//                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+//                child: Row(
+//                  children: <Widget>[
+//                    Icon(
+//                      Icons.email,
+//                      color: Colors.teal,
+//                    ),
+//                    SizedBox(
+//                      width: 10,
+//                    ),
+//                    Text(
+//                      'iamgod6666@gmail.com',
+//                      style: TextStyle(
+//                        color: Colors.teal.shade900,
+//                        fontFamily: 'Source Sans Pro',
+//                        fontSize: 20,
+//                      ),
+//                    ),
+//                  ],
+//                ),
+//              ),
+            ],
           ),
         ),
-//        floatingActionButton: FloatingActionButton(
-//          backgroundColor: Colors.red,
-//          child: Icon(Icons.add),
-//        ),
       ),
     );
   }
